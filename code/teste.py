2094039dd250c3894a581cf45ae0dbdad8ac561b
Dairton Silva <dairton@outlook.com>
2024-01-09 21:13:30 -0300

    Update teste.py

--- code/teste.py
+++ code/teste.py
@@ -1,3 +1,19 @@
 print("Essa é a soma de 1+1!")
 print(1+1)
 print(2+2)
+
+dairton = True
+
+
+if dairton == True:
+    print("Dairton")
+else:
+    print("TESTE")
+    
+if dairton:
+    print("É verdadeiro")
+else:
+    print("TESTE")
+    
+while True:
+    print(1)
